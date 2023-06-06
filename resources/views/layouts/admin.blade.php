fa1ed19e2b3894bbe142e87d6ac318049d9490c9
PietroSavini <savinipietro@gmail.com>
2023-06-06 18:45:04 +0200

    removed link to home from dashboard in logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,12 +25,10 @@
 
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <div class="logo_laravel">
-                       <h3>Dashboard</h3>
-                    </div>
-                    {{-- config('app.name', 'Laravel') --}}
-                </a>
+                
+                <div class="logo_laravel">
+                    <h3>Dashboard</h3>
+                </div>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

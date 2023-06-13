a3f080d79c25978c90370462118a0f3e14b6d116
PietroSavini <savinipietro@gmail.com>
2023-06-13 17:00:51 +0200

    ADD: public/storage & col to projects table

--- resources/views/admin/adminDashboardHome.blade.php
+++ resources/views/admin/adminDashboardHome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="ms_container">
     <h1>Bentornato {{ $user-> name }}!</h1>
-    <p>questa è l'area personale dedicata agli amministratori, da qui potrai modificare i contenuti visuaizzati in pagina o crearne di nuovi!</p>
+    <p>questa è l'area personale dedicata agli amministratori, da qui potrai modificare i contenuti visualizzati in pagina o crearne di nuovi!</p>
     <a href="{{route('admin.projects.index')}}"class="btn  btn-primary" >Inizia subito</a>
 </div>
     

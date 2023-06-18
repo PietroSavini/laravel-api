c0b73175024d116423f7810980811380c9ebf585
PietroSavini <savinipietro@gmail.com>
2023-06-19 00:05:16 +0200

    ADD: api for single project

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -15,4 +15,12 @@ class ApiController extends Controller
             'results'=> $projects,
         ]);
     }
+
+    public function show($slug){
+        $project = Project::where('slug',$slug)->first();
+        return response()->json([
+            'success'=> true,
+            'results'=> $project
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/projects',[ApiController::class,'index']);
+Route::get('projects/{slug}',[ApiController::class,'show']);
